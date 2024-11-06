5de8ce7fbaf81043f8e6e8c834d524a5d27c8c21
hoang.quoc.viet <hoang.quoc.viet@vareal.vn>
2024-11-06 14:02:45 +0700

    fix about image just use 1

--- src/app/about/images.ts
+++ src/app/about/images.ts
@@ -3,13 +3,13 @@ import { unstable_cache as cache } from 'next/cache';
 const imagesAlts: Array<string> = [
   "At Festival Estéreo Picnic – Mar '23",
   "At The Angel of Independence in Mexico City – Oct '23",
-  "In Valle de Bravo, México – Oct '23",
+  "At Savvycom running program",
   "In Medellin, Vietnam – Nov '23",
 ];
 
 export const getAboutImage = cache(
   async () => {
-    const index = Math.floor(Math.random() * imagesAlts.length);
+    const index = 2;
     const src = await import(`../../assets/images/about/${index}.jpeg`);
     console.log('src :>> ', src);
     return {
